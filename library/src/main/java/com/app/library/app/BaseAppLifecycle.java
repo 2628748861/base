60a20d5480c43b6a6226de20effe9dbb8e412c2d
fu <fuyongjun@gdgyun.com>
2019-01-09 16:41:14 +0800

    修改提示

--- library/src/main/java/com/app/library/app/BaseAppLifecycle.java
+++ library/src/main/java/com/app/library/app/BaseAppLifecycle.java
@@ -11,7 +11,7 @@ import com.app.library.frame.permission.RxPermissionAPI;
 public class BaseAppLifecycle implements AppLifecycle {
     @Override
     public void onAttach(Context applicationContext) {
-        Log.e("TAG","初始化basemoudle");
+        Log.e("TAG","初始化:"+BaseAppLifecycle.class.getSimpleName());
         ImageLoaderProxy.init(new GlideImageLoaderAPI());
         PermissionAPIProxy.init(new RxPermissionAPI());
     }
